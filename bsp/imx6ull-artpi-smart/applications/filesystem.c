30e05b3c9b6197dd6c289354e13de58c7a7560c1
@lin12345678 <linzhenxing@rt-thread.com>
2021-07-14 16:37:49 +0800

    修改一些编译告警

--- bsp/imx6ull-artpi-smart/applications/filesystem.c
+++ bsp/imx6ull-artpi-smart/applications/filesystem.c
@@ -2,6 +2,7 @@
 
 #include <dfs_fs.h>
 #include <ioremap.h>
+#include <drv_sdio.h>
 
 #define DBG_TAG "app.filesystem"
 #define DBG_LVL DBG_INFO
---
+++ bsp/imx6ull-artpi-smart/drivers/drv_sdio.h
@@ -0,0 +1,6 @@
+#ifndef __DRV_SDIO_H__
+#define __DRV_SDIO_H__
+
+void host_change(void);
+
+#endif
