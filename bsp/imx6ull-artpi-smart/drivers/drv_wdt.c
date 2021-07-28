d6332576e98d1db83c844a62fa9a416496e8359d
宋超 <songchao@rt-thread.com>
2021-07-28 17:27:20 +0800

    update code style

--- bsp/imx6ull-artpi-smart/drivers/drv_wdt.c
+++ bsp/imx6ull-artpi-smart/drivers/drv_wdt.c
@@ -19,19 +19,19 @@
 
 enum
 {
-    #ifdef RT_USING_WDT1
-    eDevWdt_WDT1,
-    #endif
+#ifdef RT_USING_WDT1
+    DEV_WDT1,
+#endif
 
-    #ifdef RT_USING_WDT2
-    eDevWdt_WDT2,
-    #endif
+#ifdef RT_USING_WDT2
+    DEV_WDT2,
+#endif
 
-    #ifdef RT_USING_WDT3
-    eDevWdt_WDT3,
-    #endif
+#ifdef RT_USING_WDT3
+    DEV_WDT3,
+#endif
 
-    eDevWdt_Max,
+    DEV_MAX,
 };
 
 #ifdef RT_USING_WDT1
@@ -40,7 +40,7 @@ static wdog_config_t WDOG_1_config =
     .timeoutValue = 0xffu,
     .enablePowerDown = false,
     .softwareResetExtension = false,
-    .softwareAssertion= true,
+    .softwareAssertion = true,
     .softwareResetSignal = true,
     .enableWdog = true,
     .workMode =
@@ -60,7 +60,7 @@ static wdog_config_t WDOG_2_config =
     .timeoutValue = 0xffu,
     .enablePowerDown = false,
     .softwareResetExtension = false,
-    .softwareAssertion= true,
+    .softwareAssertion = true,
     .softwareResetSignal = true,
     .enableWdog = true,
     .workMode =
@@ -80,7 +80,7 @@ static wdog_config_t WDOG_3_config =
     .timeoutValue = 0xffu,
     .enablePowerDown = false,
     .softwareResetExtension = false,
-    .softwareAssertion= true,
+    .softwareAssertion = true,
     .softwareResetSignal = true,
     .enableWdog = true,
     .workMode =
@@ -94,41 +94,40 @@ static wdog_config_t WDOG_3_config =
 };
 #endif
 
-static rt_watchdog_t imx6ull_watchdog[eDevWdt_Max] =
+static rt_watchdog_t imx6ull_watchdog[DEV_MAX] =
 {
-    #ifdef RT_USING_WDT1
+#ifdef RT_USING_WDT1
     {
         .name = "wdt1",
         .paddr = IMX6ULL_WATCHDOG1_BASE,
         .config = &WDOG_1_config,
     },
-    #endif
+#endif
 
-    #ifdef RT_USING_WDT2
+#ifdef RT_USING_WDT2
     {
         .name = "wdt2",
         .paddr = IMX6ULL_WATCHDOG2_BASE,
         .config = &WDOG_2_config,
     },
-    #endif
+#endif
 
-    #ifdef RT_USING_WDT3
+#ifdef RT_USING_WDT3
     {
         .name = "wdt3",
         .paddr = IMX6ULL_WATCHDOG3_BASE,
         .config = &WDOG_3_config,
     },
-    #endif
+#endif
 
 };
 
 static rt_err_t imx6ull_wdog_init(rt_watchdog_t *wdt)
 {
-    WDOG_Type *base;
+    WDOG_Type *base = RT_NULL;
     base = (WDOG_Type *)wdt->vaddr;
     WDOG_Init(base, wdt->config);
     WDOG_Disable(base);
-
     return RT_EOK;
 }
 
@@ -136,7 +135,7 @@ static rt_err_t imx6ull_wdog_control(rt_watchdog_t *wdt, int cmd, void *args)
 {
     RT_ASSERT(wdt != NULL);
 
-    WDOG_Type *base;
+    WDOG_Type *base = RT_NULL;
     base = (WDOG_Type *)wdt->vaddr;
 
     switch(cmd)
@@ -184,11 +183,10 @@ static struct rt_watchdog_ops imx6ull_wdog_ops =
 int rt_hw_wdt_init(void)
 {
     rt_err_t ret = RT_EOK;
-    for (int idx=0; idx<GET_ARRAY_NUM(imx6ull_watchdog); idx++)
+    for(int idx=0; idx<GET_ARRAY_NUM(imx6ull_watchdog); idx++)
     {
         imx6ull_watchdog[idx].ops = &imx6ull_wdog_ops;
         imx6ull_watchdog[idx].vaddr = platform_get_periph_vaddr(imx6ull_watchdog[idx].paddr);
-
         ret = rt_hw_watchdog_register(&imx6ull_watchdog[idx], imx6ull_watchdog[idx].name, RT_DEVICE_FLAG_DEACTIVATE, RT_NULL);
         if (ret != RT_EOK)
         {
