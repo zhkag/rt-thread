8a4968b991b22fa4879e15497584d4bd488fc22b
fang wei <fangwei@rt-thread.com>
2021-08-18 18:34:58 +0800

    add bsp support for qemu virt board aarch64

---
+++ bsp/qemu-virt64-aarch64/drivers/board.h
@@ -0,0 +1,72 @@
+/*
+ * File      : board.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2013, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2013-07-06     Bernard    the first version
+ */
+
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <rtconfig.h>
+#include <stdlib.h>
+#include "mmu.h"
+#include "ioremap.h"
+
+extern unsigned char __bss_start;
+extern unsigned char __bss_end;
+
+#define HEAP_BEGIN      ((void*)&__bss_end)
+
+#ifdef RT_USING_USERSPACE
+#define HEAP_END        ((size_t)KERNEL_VADDR_START + 16 * 1024 * 1024)
+#define PAGE_START      HEAP_END
+#define PAGE_END        ((size_t)KERNEL_VADDR_START + 128 * 1024 * 1024)
+#else
+#define HEAP_END        ((size_t)0x40000000 + 64 * 1024 * 1024)
+#endif
+
+#define __REG32(x)  (*((volatile unsigned int *)(x)))
+#define __REG16(x)  (*((volatile unsigned short *)(x)))
+
+/* UART PL011 */
+#define PL011_UARTDR                0x000
+#define PL011_UARTFR                0x018
+#define PL011_UARTFR_TXFF_BIT       5
+#define PL011_UART0_BASE            0x09000000
+#define PL011_UART0_SIZE            0x00001000
+
+/* GIC PL390 DIST and CPU */
+#define GIC_PL390_DISTRIBUTOR_PPTR  0x08000000
+#define GIC_PL390_CONTROLLER_PPTR   0x08010000
+
+#define MAX_HANDLERS     96
+#define GIC_IRQ_START   0 
+/* number of interrupts on board */
+#define ARM_GIC_NR_IRQS     96
+/* only one GIC available */
+#define ARM_GIC_MAX_NR      1
+
+/* the basic constants and interfaces needed by gic */
+rt_inline rt_uint64_t platform_get_gic_dist_base(void)
+{
+    return GIC_PL390_DISTRIBUTOR_PPTR;
+}
+
+rt_inline rt_uint64_t platform_get_gic_cpu_base(void)
+{
+    return GIC_PL390_CONTROLLER_PPTR;
+}
+
+void rt_hw_board_init(void);
+
+extern rt_mmu_info mmu_info;
+
+#endif
---
+++ bsp/qemu-virt64-aarch64/rtconfig.py
@@ -0,0 +1,45 @@
+import os
+
+# toolchains options
+ARCH        ='aarch64'
+CPU         ='cortex-a'
+CROSS_TOOL  = 'gcc'
+PLATFORM    = 'gcc'
+EXEC_PATH   = os.getenv('RTT_EXEC_PATH') or '/usr/bin'
+BUILD       = 'debug'
+
+if PLATFORM == 'gcc':
+    # toolchains
+    PREFIX  = os.getenv('RTT_CC_PREFIX') or 'aarch64-linux-musleabi-'
+    CC      = PREFIX + 'gcc'
+    CXX     = PREFIX + 'g++'
+    AS      = PREFIX + 'gcc'
+    AR      = PREFIX + 'ar'
+    LINK    = PREFIX + 'gcc'
+    TARGET_EXT = 'elf'
+    SIZE    = PREFIX + 'size'
+    OBJDUMP = PREFIX + 'objdump'
+    OBJCPY  = PREFIX + 'objcopy'
+    STRIP   = PREFIX + 'strip'
+    CFPFLAGS = ' '
+    AFPFLAGS = ' '
+    DEVICE   = ' -march=armv8-a -mtune=cortex-a53 -ftree-vectorize -ffast-math -funwind-tables -fno-strict-aliasing'
+
+    CXXFLAGS= DEVICE + CFPFLAGS + ' -Wall'
+    CFLAGS  = DEVICE + CFPFLAGS + ' -Wall -std=gnu99'
+    AFLAGS  = ' -c' + AFPFLAGS + ' -x assembler-with-cpp'
+    LFLAGS  = DEVICE + ' -Wl,--gc-sections,-Map=rtthread.map,-cref,-u,system_vectors -T link.lds' + ' -lsupc++ -lgcc'
+    CPATH   = ''
+    LPATH   = ''
+
+    if BUILD == 'debug':
+        CFLAGS   += ' -O0 -gdwarf-2'
+        CXXFLAGS += ' -O0 -gdwarf-2'
+        AFLAGS   += ' -gdwarf-2'
+    else:
+        CFLAGS   += ' -Os'
+        CXXFLAGS += ' -Os'
+    CXXFLAGS += ' -Woverloaded-virtual -fno-exceptions -fno-rtti'
+
+DUMP_ACTION = OBJDUMP + ' -D -S $TARGET > rtt.asm\n'
+POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
