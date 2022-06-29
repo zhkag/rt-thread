d048cc10246b44679aadcb1b67b464659c7d6d01
宋超 <songchao@rt-thread.com>
2022-06-29 20:00:29 +0800

    remove the tab

--- include/rtdef.h
+++ include/rtdef.h
@@ -1131,15 +1131,15 @@ struct rt_device
 #define RT_DRIVER_MATCH_DTS (1<<0)
 struct rt_device_id 
 {
-	const char *compatible;
-	void *data;
+    const char *compatible;
+    void *data;
 };
 
 struct rt_driver
 {
 #ifdef RT_USING_DEVICE_OPS    
     const struct rt_device_ops *dev_ops;
-#endif    
+#endif
     const struct filesystem_ops *fops;
     const char *name;
     enum rt_device_class_type dev_type;
@@ -1147,10 +1147,10 @@ struct rt_driver
     int device_size;
     int flag;
     const struct rt_device_id *dev_match;
-	int (*probe)(struct rt_device *dev);
+    int (*probe)(struct rt_device *dev);
     int (*probe_init)(struct rt_device *dev);
     int (*remove)(struct rt_device *dev);
-	const void *ops;	/* driver-specific operations */
+    const void *ops;    /* driver-specific operations */
 };
 typedef struct rt_driver *rt_driver_t;
 
