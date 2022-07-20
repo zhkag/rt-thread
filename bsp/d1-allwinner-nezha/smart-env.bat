8cdbfdfe5cc9fb99fdd0f6e83802aa90abd31dbe
张世争 <zhangshizheng@rt-thread.com>
2022-07-20 13:59:20 +0800

    [bsp/d1-allwinner-nezha] add compile tools script

---
+++ bsp/d1-allwinner-nezha/smart-env.bat
@@ -0,0 +1,9 @@
+set RTT_CC=gcc
+set RTT_EXEC_PATH=%cd%\tools\gnu_gcc\riscv64-linux-musleabi_for_i686-w64-mingw32\bin
+set RTT_CC_PREFIX=riscv64-unknown-linux-musl-
+@set PATH=%RTT_EXEC_PATH%;%ENV_ROOT%\tools\gnu_gcc\arm_gcc\mingw\bin;%PATH%
+
+@echo "Arch      : riscv64"
+@echo "CC        : %RTT_CC%"
+@echo "PREFIX    : %RTT_CC_PREFIX%"
+@echo "EXEC_PATH : %RTT_EXEC_PATH%"
