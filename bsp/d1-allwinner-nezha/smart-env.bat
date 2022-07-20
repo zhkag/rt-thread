6535d3ef45066c08e09e30ed28bc1d6e63600f55
张世争 <zhangshizheng@rt-thread.com>
2022-07-20 15:00:52 +0800

    windows 下 mkimage 脚本

--- bsp/d1-allwinner-nezha/smart-env.bat
+++ bsp/d1-allwinner-nezha/smart-env.bat
@@ -1,7 +1,8 @@
 set RTT_CC=gcc
 set RTT_EXEC_PATH=%cd%\tools\gnu_gcc\riscv64-linux-musleabi_for_i686-w64-mingw32\bin
 set RTT_CC_PREFIX=riscv64-unknown-linux-musl-
-@set PATH=%RTT_EXEC_PATH%;%ENV_ROOT%\tools\gnu_gcc\arm_gcc\mingw\bin;%PATH%
+set RTT_TOOLS_PATH=%cd%\tools
+@set PATH=%RTT_EXEC_PATH%;%RTT_TOOLS_PATH%;%ENV_ROOT%\tools\gnu_gcc\arm_gcc\mingw\bin;%PATH%
 
 @echo "Arch      : riscv64"
 @echo "CC        : %RTT_CC%"
