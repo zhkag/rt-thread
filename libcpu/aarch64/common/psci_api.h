a24fa3c032ebf4ae9e513699a697dda0f130b16d
wangxiaoyao <wangxiaoyao@rt-thread.com>
2022-08-15 17:07:49 +0800

    [libcpu/aarch64] 增加基本psci协议支持

---
+++ libcpu/aarch64/common/psci_api.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+#ifndef __PSCI_API_H__
+#define __PSCI_API_H__
+
+#include <rthw.h>
+#include <rtthread.h>
+#include <stdint.h>
+#include "psci_api.h"
+
+/** generic psci ops supported v0.1 v0.2 v1.0 v1.1 */
+struct psci_operations
+{
+    uint32_t (*get_version)(void);
+    int32_t (*cpu_suspend)(uint32_t state, unsigned long entry_point);
+    int32_t (*cpu_off)(uint32_t state);
+    int32_t (*cpu_on)(unsigned long cpuid, unsigned long entry_point);
+    int32_t (*migrate)(unsigned long cpuid);
+};
+
+extern struct psci_operations psci_ops;
+
+extern int psci_init(void);
+
+#endif // __PSCI_API_H__
